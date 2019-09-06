c781692be397d4dcb43f7c6a0bf88bc4bc307a49
cdgeorge7 <cdgeorge7@gmail.com>
2019-09-06 14:44:19 -0500

    pulling lineups from api and rendering using useEffect

--- src/components/LineupGroup.js
+++ src/components/LineupGroup.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Lineup from "./Lineup";
 import uuid4 from "uuid";
+/* import axios from "axios"; */
 
-import dummyLineupsData from "./dummyLineupsData";
+/* import dummyLineupsData from "./dummyLineupsData"; */
 
 export default function LineupGroup() {
-  const lineups = dummyLineupsData.map(lineup => (
+  /*   const lineups = dummyLineupsData.map(lineup => (
     <Lineup
       key={uuid4()}
       id={lineup.lineup_id}
       players={lineup.players}
       points={lineup.lineup_points}
     />
-  ));
+  )); */
+
+  const [lineups, setLineups] = useState([]);
+  const [hasError, setError] = useState(false);
+
+  const LINEUPS_URL = "http://localhost:5000/lineups";
+  async function fetchData() {
+    await fetch(LINEUPS_URL)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Error happened");
+        }
+      })
+      .then(resJSON => {
+        /* console.log(JSON.parse(res).lineups); */
+        setLineups(
+          JSON.parse(resJSON).lineups.map(lineup => (
+            <Lineup
+              key={uuid4()}
+              id={lineup.lineup_id}
+              players={lineup.players}
+              points={lineup.lineup_points}
+            />
+          ))
+        );
+      })
+      .catch(err => {
+        console.log(err);
+        setError(true);
+      });
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  /* axios.get(LINEUPS_URL).then(res => {useState({
+    res.data.lineups.map(lineup => (
+      <Lineup
+        key={uuid4()}
+        id={lineup.lineup_id}
+        players={lineup.players}
+        points={lineup.lineup_points}
+      />
+    ));
+    return {lineups};
+  })}); */
 
   const rows = [];
   const LINEUPS_PER_ROW = 3;
@@ -36,5 +83,5 @@ export default function LineupGroup() {
       );
     }
   }
-  return <div>{rows}</div>;
+  return hasError ? <div>error</div> : <div>{rows}</div>;
 }
