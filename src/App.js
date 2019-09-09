391f457e3fa1242db69124910782942bd052f6b1
cdgeorge7 <cdgeorge7@gmail.com>
2019-09-08 21:59:42 -0500

    branch commit test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <div className="container">
         <AppOptions />
-        <LineupGroup refresh={false} refreshRate={5000} />
+        <LineupGroup refresh={true} refreshRate={5000} />
       </div>
     </div>
   );
